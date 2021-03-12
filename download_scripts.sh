57220d60d1c92389461e37bbd2b83b232a3b5c80
Anthony <anthony.piccinone@gmail.com>
2021-03-12 00:47:24 -0500

    Added script to download other scripts

---
+++ download_scripts.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+curl -L https://raw.githubusercontent.com/Zombant/InstallArch/master/arch_install.sh > arch_install.sh
+curl -L https://raw.githubusercontent.com/Zombant/InstallArch/master/inside_system.sh > inside_system.sh
+curl -L https://raw.githubusercontent.com/Zombant/InstallArch/master/other_packages.sh > other_packages.sh
