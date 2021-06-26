5c0ee0c1ed41270bb16ecd2a89edc1c37c26e373
Anthony Piccinone <anthony.piccinone@gmail.com>
2021-06-26 11:17:15 -0400

    Added agate-bin and changed AUR packages to a list

--- other_packages.sh
+++ other_packages.sh
@@ -87,208 +87,33 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 # Install .NET Core and mono
 pacman -S dotnet-runtime dotnet-sdk mono-msbuild mono --noconfirm
 
-### This section downloads from the AUR
+### AUR packages to install
+# agate-bin
+# android-studio
+# balena-etcher
+# blugon
+# brave-bin
+# joplin-desktop
+# minecraft-launcher
+# moc-pulse-svn
+# nerd-fonts-mononoki
+# nerd-fonts-roboto-mono
+# openrazer-driver-dkms
+# paru
+# python-spotdl
+# shell-color-scripts
+# snapd
+# ttf-ms-fonts
+# ttf-vista-fonts
+# unityhub
+# zoom
 
-# Download paru
-#git clone https://aur.archlinux.org/paru.git /home/${1}/paru
-#chown ${1} /home/${1}/paru
-
-# Download shell-color-scripts
-git clone https://aur.archlinux.org/shell-color-scripts.git /home/${1}/shell-color-scripts
-chown ${1} /home/${1}/shell-color-scripts
-
-# Download android-studio
-git clone https://aur.archlinux.org/android-studio.git /home/${1}/android-studio
-chown ${1} /home/${1}/android-studio
-
-# Download zoom
-git clone https://aur.archlinux.org/zoom.git /home/${1}/zoom
-chown ${1} /home/${1}/zoom
-
-# Download minecraft-launcher
-git clone https://aur.archlinux.org/minecraft-launcher.git /home/${1}/minecraft-launcher
-chown ${1} /home/${1}/minecraft-launcher
-
-# Download chrome-remote-desktop
-git clone https://aur.archlinux.org/chrome-remote-desktop.git /home/${1}/chrome-remote-desktop
-chown ${1} /home/${1}/chrome-remote-desktop
-
-# Download brave-bin
-git clone https://aur.archlinux.org/brave-bin.git /home/${1}/brave-bin
-chown ${1} /home/${1}/brave-bin
-
-# Download chrome-remote-desktop
-git clone https://aur.archlinux.org/chrome-remote-desktop.git /home/${1}/chrome-remote-desktop
-chown ${1} /home/${1}/brave-bin
-
-# Download nerd-fonts-mononoki
-git clone https://aur.archlinux.org/nerd-fonts-mononoki.git /home/${1}/nerd-fonts-mononoki
-chown ${1} /home/${1}/nerd-fonts-mononoki
-
-# Download nerd-fonts-roboto-mono
-git clone https://aur.archlinux.org/nerd-fonts-roboto-mono.git /home/${1}/nerd-fonts-roboto-mono
-chown ${1} /home/${1}/nerd-fonts-roboto-mono
-
-# Download joplin-desktop
-git clone https://aur.archlinux.org/joplin.git /home/${1}/joplin
-chown ${1} /home/${1}/joplin
-
-# Download openrazer-driver-dkms
-git clone https://aur.archlinux.org/openrazer.git
-chown ${1} /home/${1}/openrazer
-
-# Download balena-etcher
-git clone https://aur.archlinux.org/balena-etcher.git
-chown ${1} /home/${1}/balena-etcher
-
-# Download unityhub
-git clone https://aur.archlinux.org/unityhub.git
-chown ${1} /home/${1}/unityhub
-
-# Download microsoft fonts
-git clone https://aur.archlinux.org/ttf-ms-fonts.git
-git clone https://aur.archlinux.org/ttf-vista-fonts.git
-chown ${1} /home/${1}/ttf-ms-fonts
-chown ${1} /home/${1}/ttf-vista-fonts
-
-# Download snapd
-git clone https://aur.archlinux.org/snapd.git
-chown ${1} /home/${1}/snapd
-
-# Download blugon
-git clone https://aur.archlinux.org/blugon.git
-chown ${1} /home/${1}/blugon
-
-# Download gmi2html
-git clone https://aur.archlinux.org/gmi2html.git /home/${1}/gmi2html
-chown ${1} /home/${1}/gmi2html
-
-git clone https://aur.archlinux.org/newsflash.git /home/${1}/newsflash
-chown ${1} /home/${1}/newsflash
-
-git clone https://aur.archlinux.org/shortwave.git /home/${1}/shortwave
-chown ${1} /home/${1}/shortwave
-
-git clone https://aur.archlinux.org/moc-pulse-svn.git /home/${1}/moc-pulse-svn
-chown ${1} /home/${1}/moc-pulse-svn
-
-
-chown ${1} -R /home/${1}
 
 ###
 
 # Run as user
 su $1 <<EOF
 
-	### This section installs downloaded AUR packages
-	# Install paru
-	#cd /home/${1}/paru
-	#makepkg -sri --noconfirm
-	# Sync AUR
-	#paru -Syu
-	
-	# Install shell-color-scripts
-	cd /home/${1}/shell-color-scripts
-	makepkg -sri --noconfirm
-
-	# Install android-studio
-	cd /home/${1}/android-studio
-	makepkg -sri --noconfirm
-
-	# Install zoom
-	cd /home/${1}/zoom
-	makepkg -sri --noconfirm
-	
-	# Install minecraft-launcher
-	cd /home/${1}/minecraft-launcher
-	makepkg -sri --noconfirm
-
-	# Install chrome-remote-desktop
-	cd /home/${1}/chrome-remote-desktop
-	makepkg -sri --noconfirm
-
-	# Install brave-bin
-	cd /home/${1}/brave-bin
-	makepkg -sri --noconfirm
-
-	# Install nerd-fonts-mononoki
-	cd /home/${1}/nerd-fonts-mononoki
-	makepkg -sri --noconfirm
-
-	# Install nerd-fonts-roboto-mono
-	cd /home/${1}/nerd-fonts-roboto-mono
-	makepkg -sri --noconfirm
-
-	# Install joplin
-	cd /home/${1}/joplin
-	makepkg -sri --noconfirm
-
-   	# Install openrazer-driver-dkms
-    	cd /home/${1}/openrazer
-    	makepkg -sri --noconfirm
-
-    	# Install balena-etcher
-    	cd /home/${1}/balena-etcher
-    	makepkg -sri --noconfirm
-    	
-    	# Install unityhub
-    	cd /home/${1}/unityhub
-    	makepkg -sri --noconfirm
-
-    	# Install microsoft fonts
-    	cd /home/${1}/ttf-ms-fonts
-    	makepkg -sri --noconfirm
-    	cd /home/${1}/ttf-vista-fonts
-    	makepkg -sri --noconfirm
-    
-    	# Install snapd
-    	cd /home/${1}/snapd
-    	makepkg -sri --noconfirm
-
-        # Install blugon
-    	cd /home/${1}/blugon
-    	makepkg -sri --noconfirm
-
-        # Install gmi2html
-    	cd /home/${1}/gmi2html
-    	makepkg -sri --noconfirm
-
-	# Install newsflash
-    	cd /home/${1}/newsflash
-    	makepkg -sri --noconfirm
-
-	# Install shortwave
-    	cd /home/${1}/shortwave
-    	makepkg -sri --noconfirm
-
-	# Install moc-pulse-svn
-    	cd /home/${1}/moc-pulse-svn
-    	makepkg -sri --noconfirm
-
-
-	# Clean up home directory
-	cd /home/${1}
-	rm -rf paru/
-	rm -rf shell-color-scripts/
-	rm -rf android-studio/
-	rm -rf zoom/
-	rm -rf minecraft-launcher/
-	rm -rf chrome-remote-desktop/
-	rm -rf brave-bin/
-	rm -rf nerd-fonts-mononoki/
-	rm -rf nerd-fonts-roboto-mono/
-	rm -rf joplin/
-   	rm -rf openrazer/
-    	rm -rf balena-etcher/
-    	rm -rf ttf-ms-fonts/
-    	rm -rf ttf-vista-fonts
-    	rm -rf snapd/
-    	rm -rf blugon/
-    	rm -rf gmi2html/
-	rm -rf newsflash/
-	rm -rf shortwave/
-	rm -rf moc-pulse-svn
-	###
 
 EOF
 
