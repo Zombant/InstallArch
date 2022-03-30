99a10e89a9f98670da828ab202a6130948561292
Anthony Piccinone <anthony.piccinone@gmail.com>
2022-03-30 13:27:42 -0400

    Added udev rules for brightness on flex

--- other_packages.sh
+++ other_packages.sh
@@ -14,9 +14,6 @@ pacman -S xlockmore --noconfirm
 # File systems
 pacman -S ntfs-3g cifs-utils fuse --noconfirm
 
-# Policy Kit
-pacman -S lxsession --noconfirm
-
 # Set up pulseaudio
 pacman -S pulseaudio pulseaudio-alsa alsa-utils pavucontrol --noconfirm
 pulseaudio --check
@@ -37,10 +34,6 @@ pacman -S maim --noconfirm
 # Fonts
 pacman -S ttf-ubuntu-font-family noto-fonts-emoji xorg-fonts-misc xorg-xlsfonts xorg-xfontsel --noconfirm
 
-# Themes and icons
-pacman -S lxappearance qt5ct breeze-gtk breeze-icons arc-gtk-theme arc-icon-theme --noconfirm
-wget -qO- https://git.io/papirus-icon-theme-install | sh
-
 # Wallpapers
 pacman -S nitrogen archlinux-wallpaper livewallpaper --noconfirm
 
@@ -51,7 +44,7 @@ pacman -S dunst libnotify --noconfirm
 pacman -S network-manager-applet pasystray --noconfirm
 
 # Compression
-pacman -S zip unzip atool --noconfirm
+pacman -S zip unzip --noconfirm
 
 # Android phone
 pacman -S android-file-transfer scrcpy --noconfirm
@@ -70,11 +63,18 @@ pacman -S virtualbox virtualbox-host-modules-arch --noconfirm
 pacman -S udisks2 --noconfirm
 
 # Changing brightness
-# For intel
+# For HP Pavilion
 # mkdir -p /etc/udev/rules.d
 # touch /etc/udev/rules.d/backlight.rules
 # echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"intel_backlight\", RUN+=\"/usr/bin/chgrp video /sys/class/backlight/intel_backlight/brightness\"" > /etc/udev/rules.d/backlight.rules
 # echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"intel_backlight\", RUN+=\"/usr/bin/chmod g+w /sys/class/backlight/intel_backlight/brightness\"" >> /etc/udev/rules.d/backlight.rules
+
+# For Lenovo Flex
+#mkdir -p /etc/udev/rules.d
+#touch /etc/udev/rules.d/backlight.rules
+#echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"amdgpu_b10\", RUN+=\"/usr/bin/chrgrp video /sys/class/backlight/amdgpu_b10/brightness\"" > /etc/udev/rules.d/backlight.rules
+#echo "ACTION==\"add\", SUBSYSTEM==\"backlight\", KERNEL==\"amdgpu_b10\", RUN+=\"/usr/bin/chmod g+w /sys/class/backlight/amdgpu_b10/brightness\"" >> /etc/udev/rules.d/backlight.rules
+
 
 ## Window managers
 
@@ -99,8 +99,13 @@ pacman -S plank --noconfirm
 
 # Misc.
 # ttf-liberation is a font for steam
+#dvdstyler
 pacman -Syu --noconfirm
-pacman -S grub-customizer libreoffice-fresh jre-openjdk steam ttf-liberation java-runtime discord stellarium wireshark-qt iftop macchanger calcurse exa bat ripgrep tokei procs prettyping dvdstyler nmap youtube-dl shellcheck wget gpick xorg-xclock xdotool arandr --noconfirm
+pacman -S grub-customizer libreoffice-fresh jre-openjdk steam ttf-liberation java-runtime discord stellarium wireshark-qt iftop macchanger calcurse exa bat procs prettyping nmap youtube-dl shellcheck wget gpick xorg-xclock xdotool arandr --noconfirm
+
+# Themes and icons
+pacman -S lxappearance qt5ct breeze-gtk breeze-icons arc-gtk-theme arc-icon-theme --noconfirm
+wget -qO- https://git.io/papirus-icon-theme-install | sh
 
 # Terminals
 pacman -S alacritty xterm --noconfirm
@@ -131,27 +136,28 @@ pacman -S poppler --noconfirm
 pacman -S feh --noconfirm
 
 # Media players
-pacman -S vlc mpv mplayer --noconfirm
+#mplayer
+pacman -S vlc mpv --noconfirm
 
 # Audio visualizer
 pacman -S glava --noconfirm
 
 # Browsers
-#firefox
-pacman -S qutebrowser amfora lynx --noconfirm
+#firefox lynx
+pacman -S qutebrowser amfora --noconfirm
 
 # Email
-pacman -S neomutt isync --noconfirm
+pacman -S neomutt --noconfirm
 
 # Programming/IDEs
-pacman -S nasm --noconfirm
+# pacman -S nasm --noconfirm
 pacman -S intellij-idea-community-edition pycharm-community-edition atom arduino arduino-avr-core --noconfirm
 
 # Graphics/design
-pacman -S gimp blender imagemagick --noconfirm
+pacman -S gimp blender --noconfirm
 
 # Audio editors
-pacman -S audacity mpg123 --noconfirm
+pacman -S audacity --noconfirm
 
 # Calculators
 # gcc-fortran is for octave packages
@@ -161,16 +167,16 @@ pacman -S qalculate-gtk octave gcc-fortran --noconfirm
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 # .NET Core and mono
-pacman -S dotnet-runtime dotnet-sdk mono-msbuild mono --noconfirm
+# pacman -S dotnet-runtime dotnet-sdk mono-msbuild mono --noconfirm
 
 # Torrent clients
 pacman -S qbittorrent --noconfirm
 
 # VPN
-pacman -S openvpn networkmanager-openvpn --noconfirm
+# pacman -S openvpn networkmanager-openvpn --noconfirm
 
 # Password manager
-pacman -S keepassxc pass --noconfirm
+pacman -S keepassxc --noconfirm
 
 # Cloud sync
 pacman -S nextcloud-client --noconfirm
